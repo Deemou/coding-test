1ba6a3734cee04afb63da7a254c90a31c6b9dce9
Deemou <4106243@naver.com>
2022-11-10 08:24:54 +0900

    Update 3진법 뒤집기.js

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.js
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -1,9 +1,4 @@
 function solution(n) {
-  const arr = [];
   const RADIX = 3;
-  while (n !== 0) {
-    arr.unshift(n % RADIX);
-    n = Math.floor(n / RADIX);
-  }
-  return arr.reduce((acc, cur, index) => acc + cur * Math.pow(RADIX, index), 0);
+  return parseInt(n.toString(RADIX).split("").reverse().join(""), RADIX);
 }
