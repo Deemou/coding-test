c6da82a495c23d854a4bca3c6db6eff417ae8f73
Deemou <4106243@naver.com>
2022-11-11 08:52:31 +0900

    Update 문자열 내 마음대로 정렬하기.js

--- 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -8,3 +8,13 @@ function solution(strings, n) {
     return -1;
   });
 }
+
+// String.localeCompare()를 이용한 풀이
+// 문제는 실행속도가 약 500배 이상 느렸음
+// function solution(strings, n) {
+//   return strings.sort((a, b) => {
+//     const char1 = a.charAt(n);
+//     const char2 = b.charAt(n);
+//     return char1 === char2 ? a.localeCompare(b) : char1.localeCompare(char2);
+//   });
+// }
