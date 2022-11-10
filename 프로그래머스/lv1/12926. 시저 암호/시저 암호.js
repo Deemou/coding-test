046af9784593c1894787bfbdaeb2ea8f5da989a3
Deemou <4106243@naver.com>
2022-11-10 11:03:56 +0900

    [level 1] Title: 시저 암호, Time: 3.36 ms, Memory: 37.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.js
@@ -0,0 +1,28 @@
+function solution(s, n) {
+  const arr = s.split("");
+  arr.forEach((char, index) => {
+    arr[index] = getNewChar(char, n);
+  });
+  return arr.join("");
+}
+function isAlphabet(char) {
+  return /[A-Za-z]/g.test(char);
+}
+function isUpperCase(char) {
+  return /[A-Z]/g.test(char);
+}
+function getAscii(char) {
+  return char.charCodeAt(0);
+}
+function getNewChar(char, distance) {
+  if (!isAlphabet(char)) return char;
+
+  const ALPHABET_NUM = 26;
+  distance %= ALPHABET_NUM;
+  const LAST_UPPER_ASCII = 90;
+  const LAST_LOWER_ASCII = 122;
+  const ascii = getAscii(char) + distance;
+  const lastAscii = isUpperCase(char) ? LAST_UPPER_ASCII : LAST_LOWER_ASCII;
+  const code = ascii > lastAscii ? ascii - ALPHABET_NUM : ascii;
+  return String.fromCharCode(code);
+}
