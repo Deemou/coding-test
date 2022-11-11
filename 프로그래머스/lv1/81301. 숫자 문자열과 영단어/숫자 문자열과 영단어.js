0e984937b6bc8a5241229cc517c15fc87e54c25d
Deemou <4106243@naver.com>
2022-11-11 10:53:32 +0900

    String.split()과 Array.join()을 이용한 풀이 추가

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -17,3 +17,24 @@ function solution(s) {
     }, s)
   );
 }
+
+// String.split()과 Array.join()을 이용한 풀이
+// function solution(s) {
+//   let numberWords = [
+//     "zero",
+//     "one",
+//     "two",
+//     "three",
+//     "four",
+//     "five",
+//     "six",
+//     "seven",
+//     "eight",
+//     "nine",
+//   ];
+//   return Number(
+//     numberWords.reduce((str, numberWord, index) => {
+//       return str.split(numberWord).join(index);
+//     }, s)
+//   );
+// }
