68459d26afc89ac41ef89a4138d7dd34e4392a49
Deemou <4106243@naver.com>
2022-11-11 23:07:35 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.js
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -1,21 +1,20 @@
 function solution(id_list, report, k) {
-  const reportedUsers = report.reduce((reportedUsers, reportLog) => {
-    const [user, reportedUser] = reportLog.split(" ");
-    if (!reportedUsers.has(reportedUser))
-      reportedUsers.set(reportedUser, new Set());
-
-    const usersWhoReported = reportedUsers.get(reportedUser).add(user);
-    reportedUsers.set(reportedUser, usersWhoReported);
-    return reportedUsers;
-  }, new Map());
-  const answer = new Array(id_list.length).fill(0);
-  reportedUsers.forEach((usersWhoReported) => {
-    if (usersWhoReported.size >= k) {
-      usersWhoReported.forEach((user) => {
-        const index = id_list.indexOf(user);
-        answer[index] += 1;
-      });
-    }
+  const reportLogs = [...new Set(report)].map((reportLog) => {
+    return reportLog.split(" ");
   });
-  return answer;
+  const reportedUsersCount = new Map();
+  reportLogs.forEach(([_, reportedUser]) => {
+    reportedUsersCount.set(
+      reportedUser,
+      reportedUsersCount.get(reportedUser) + 1 || 1
+    );
+  });
+
+  const numOfIncomingMails = new Array(id_list.length).fill(0);
+  for (const [user, reportedUser] of reportLogs) {
+    if (reportedUsersCount.get(reportedUser) < k) continue;
+    const index = id_list.indexOf(user);
+    numOfIncomingMails[index] += 1;
+  }
+  return numOfIncomingMails;
 }
