142265f46f36802264d3721b63f232773f8ac3f6
Deemou <4106243@naver.com>
2022-11-12 02:04:58 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.js
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.js
@@ -1,23 +1,15 @@
 function solution(ingredient) {
-  const RIGHT_ORDER = [1, 2, 3, 1];
+  const RIGHT_ORDER = "1231";
   const ORDER_LENGTH = 4;
   let count = 0;
-  let stack = [];
 
   for (let i = 0; i < ingredient.length; i++) {
-    stack.push(ingredient[i]);
-
-    if (stack.length < ORDER_LENGTH) continue;
-
-    let j = 0;
-    for (j; j < ORDER_LENGTH; j++) {
-      const index = stack.length - ORDER_LENGTH + j;
-      if (stack[index] !== RIGHT_ORDER[j]) break;
+    if (ingredient.slice(i, i + ORDER_LENGTH).join("") === RIGHT_ORDER) {
+      count++;
+      ingredient.splice(i, ORDER_LENGTH);
+      i -= ORDER_LENGTH - 1;
     }
-    if (j != ORDER_LENGTH) continue;
-
-    stack.splice(-ORDER_LENGTH);
-    count++;
   }
+
   return count;
 }
