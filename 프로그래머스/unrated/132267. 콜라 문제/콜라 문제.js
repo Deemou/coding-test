50636a4f18a9fbb6da420bb17e69195250769019
Deemou <4106243@naver.com>
2022-12-05 04:15:55 +0900

    [unrated] Title: 콜라 문제, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.js
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.js
@@ -1,13 +1,3 @@
 function solution(a, b, n) {
-  return countNumberOfBottles(a, b, n);
-}
-
-function countNumberOfBottles(required, received, own) {
-  if (own < required) return 0;
-  const numberOfTransactions = Math.round(own / required);
-  const totalReceived = numberOfTransactions * received;
-  return (
-    totalReceived +
-    countNumberOfBottles(required, received, own - required * numberOfTransactions + totalReceived)
-  );
+  return Math.floor((n - b) / (a - b)) * b;
 }
