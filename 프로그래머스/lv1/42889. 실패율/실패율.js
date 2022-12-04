9b29cd01ad0fcb31d86b1bb66a8a1dd6a9ca2c57
Deemou <4106243@naver.com>
2022-12-05 01:28:41 +0900

    [level 1] Title: 실패율, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.js
+++ 프로그래머스/lv1/42889. 실패율/실패율.js
@@ -1,65 +1,14 @@
+//간단한 버전
 function solution(N, stages) {
-  const stage = new Stage(N, stages);
-  return stage.getStagesSortedInDescendingOrderOfFailureRate();
-}
-
-class Stage {
-  #numberOfUsers;
-  #currentStageOfUsers;
-  #stages;
-
-  constructor(numberOfStages, currentStageOfUsers) {
-    this.#numberOfUsers = currentStageOfUsers.length;
-    this.#currentStageOfUsers = currentStageOfUsers;
-    this.initStages(numberOfStages);
-    this.setNumberOfCurrentUsers();
-    this.setFailureRate();
+  const NUMBER = 0;
+  const FAILURE_RATE = 1;
+  const stagesWithFailureRate = [];
+  for (let i = 1; i <= N; i++) {
+    const numberOfTotalReachedUsers = stages.filter((stage) => stage >= i).length;
+    const numberOfCurrentUsers = stages.filter((stage) => stage === i).length;
+    const failureRate = numberOfCurrentUsers / numberOfTotalReachedUsers;
+    stagesWithFailureRate.push([i, failureRate]);
   }
-
-  initStages(numberOfStages) {
-    const stages = [];
-    for (let i = 0; i <= numberOfStages; i++) {
-      const stage = this.getInitializedStage(i);
-      stages.push(stage);
-    }
-    this.#stages = stages;
-  }
-
-  getInitializedStage(number) {
-    const stage = { number: number + 1, numberOfCurrentUsers: 0, failureRate: 0 };
-    Object.seal(stage);
-    return stage;
-  }
-
-  setNumberOfCurrentUsers() {
-    this.#currentStageOfUsers.forEach((stage) => {
-      this.#stages[stage - 1].numberOfCurrentUsers += 1;
-    });
-  }
-
-  setFailureRate() {
-    let numberOfTotalReachedUsers = this.#numberOfUsers;
-
-    for (let i = 0; i < this.#stages.length; i++) {
-      const numberOfCurrentUsers = this.#stages[i].numberOfCurrentUsers;
-      const failureRate = this.getFailureRate(numberOfCurrentUsers, numberOfTotalReachedUsers);
-      numberOfTotalReachedUsers -= numberOfCurrentUsers;
-      this.#stages[i].failureRate = failureRate;
-    }
-  }
-
-  getFailureRate(numberOfCurrentUsers, numberOfTotalReachedUsers) {
-    return numberOfCurrentUsers == 0 ? 0 : numberOfCurrentUsers / numberOfTotalReachedUsers;
-  }
-
-  getStagesSortedInDescendingOrderOfFailureRate() {
-    const stagesSortedInDescendingOrderOfFailureRate = [];
-    const stages = this.#stages.slice(0, -1);
-
-    stages.sort((stage1, stage2) => stage2.failureRate - stage1.failureRate);
-    stages.forEach((stage) => {
-      stagesSortedInDescendingOrderOfFailureRate.push(stage.number);
-    });
-    return stagesSortedInDescendingOrderOfFailureRate;
-  }
+  stagesWithFailureRate.sort((stage1, stage2) => stage2[FAILURE_RATE] - stage1[FAILURE_RATE]);
+  return stagesWithFailureRate.map((stage) => stage[NUMBER]);
 }
