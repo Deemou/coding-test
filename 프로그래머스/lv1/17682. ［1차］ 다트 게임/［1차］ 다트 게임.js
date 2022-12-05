748ab8b1edd16177b00896b46db8d8bccca32a14
Deemou <4106243@naver.com>
2022-12-06 01:30:38 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.28 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.js
+++ 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.js
@@ -1,38 +1,22 @@
 function solution(dartResult) {
-  let dartNumber = -1;
-  const numberRegex = /\d{1,}/g;
+  const dartsRegex = /\d{1,}.?\D/g;
+  const dartRegex = /(\d{1,})(S|D|T)(\*|#)?/;
 
-  const dartScore = dartResult.match(numberRegex).map((score) => Number(score));
+  const bonuses = { S: 1, D: 2, T: 3 },
+    options = { '*': 2, '#': -1, undefined: 1 };
 
-  for (let i = 0; i < dartResult.length; i++) {
-    const char = dartResult[i];
-    if (isBonus(char)) {
-      dartNumber += 1;
-      const bonusPower = getBonusPower(char);
-      dartScore[dartNumber] = Math.pow(dartScore[dartNumber], bonusPower);
-    } else if (isOption(char)) {
-      if (char == '*') {
-        dartScore[dartNumber] *= 2;
-        dartScore[dartNumber - 1] *= 2;
-      } else if (char == '#') dartScore[dartNumber] *= -1;
-    }
+  const darts = dartResult.match(dartsRegex);
+
+  for (let i = 0; i < darts.length; i++) {
+    const split = darts[i].match(dartRegex);
+    const baseScore = split[1];
+    const bonus = split[2];
+    const option = split[3];
+    const score = Math.pow(baseScore, bonuses[bonus]) * options[option];
+
+    if (option === '*' && darts[i - 1]) darts[i - 1] *= options['*'];
+    darts[i] = score;
   }
 
-  let totalScore = 0;
-  dartScore.forEach((score) => (totalScore += score));
-  return totalScore;
-}
-
-function isBonus(bonus) {
-  return /[SDT]/g.test(bonus);
-}
-
-function isOption(option) {
-  return /[*#]/g.test(option);
-}
-
-function getBonusPower(bonus) {
-  if (bonus == 'S') return 1;
-  if (bonus == 'D') return 2;
-  if (bonus == 'T') return 3;
+  return darts.reduce((totalScore, score) => totalScore + score);
 }
