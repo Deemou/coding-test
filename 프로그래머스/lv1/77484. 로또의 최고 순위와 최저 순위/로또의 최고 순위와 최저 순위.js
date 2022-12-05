e0a6a023417e5de382308233465ea3409c5c3d4e
Deemou <4106243@naver.com>
2022-12-06 07:30:58 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.06 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -0,0 +1,14 @@
+function solution(lottos, win_nums) {
+  const prize = [6, 6, 5, 4, 3, 2, 1];
+  let hitCount = 0;
+  let unknownCount = 0;
+
+  lottos.forEach((lottoNumber) => {
+    if (lottoNumber == 0) unknownCount++;
+    if (win_nums.includes(lottoNumber)) hitCount++;
+  });
+
+  const maxCount = Math.min(6, hitCount + unknownCount);
+
+  return [prize[maxCount], prize[hitCount]];
+}
