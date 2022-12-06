99876e032b1584b3db84f48e83b697922b2cd7c0
Deemou <4106243@naver.com>
2022-12-06 23:00:55 +0900

    [level 1] Title: 체육복, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.js
+++ 프로그래머스/lv1/42862. 체육복/체육복.js
@@ -1,25 +1,26 @@
 function solution(n, lost, reserve) {
-    const students = {};
-    let answer = 0;
-    for(let i = 1; i <= n; i++){
-        students[i] = 1;
-    }
-    lost.forEach(number => students[number] -= 1);
-    reserve.forEach(number => students[number] += 1);
+  const numberOfClothesByStudent = new Array(n).fill(1);
+  lost.forEach((studentNumber) => (numberOfClothesByStudent[studentNumber - 1] -= 1));
+  reserve.forEach((studentNumber) => (numberOfClothesByStudent[studentNumber - 1] += 1));
 
-    for(let i = 1; i <= n; i++){
-        if(students[i] === 2 && students[i-1] === 0){
-                students[i-1]++;
-                students[i]--;
-        } else if(students[i] === 2 && students[i+1] === 0){
-                students[i+1]++;
-                students[i]--;
-        }
+  for (let studentNumber = 0; studentNumber < n; studentNumber++) {
+    if (
+      numberOfClothesByStudent[studentNumber] === 2 &&
+      numberOfClothesByStudent[studentNumber - 1] === 0
+    ) {
+      numberOfClothesByStudent[studentNumber - 1]++;
+      numberOfClothesByStudent[studentNumber]--;
+    } else if (
+      numberOfClothesByStudent[studentNumber] === 2 &&
+      numberOfClothesByStudent[studentNumber + 1] === 0
+    ) {
+      numberOfClothesByStudent[studentNumber + 1]++;
+      numberOfClothesByStudent[studentNumber]--;
     }
-    for(let key in students){
-        if(students[key] >= 1){
-            answer++;
-        }
-    }
-    return answer;
+  }
+
+  return numberOfClothesByStudent.reduce((count, numberOfClothes) => {
+    if (numberOfClothes >= 1) count++;
+    return count;
+  }, 0);
 }
