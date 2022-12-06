32e3ba1af814f88844049b6cbe3dc72481041c3f
Deemou <4106243@naver.com>
2022-12-07 01:12:35 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 92.33 ms, Memory: 56.9 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,7 +1,5 @@
 function solution(participant, completion) {
   participant.sort();
   completion.sort();
-  for (let i = 0; i < participant.length; i++) {
-    if (participant[i] != completion[i]) return participant[i];
-  }
+  return participant.find((name, index) => name != completion[index]);
 }
