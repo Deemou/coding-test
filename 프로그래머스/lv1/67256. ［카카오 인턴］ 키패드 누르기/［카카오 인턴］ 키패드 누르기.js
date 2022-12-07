9b78224ac40ce99d02562939b5d4f6d1c928ebe9
Deemou <4106243@naver.com>
2022-12-07 23:59:00 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 1.48 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.js
+++ 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.js
@@ -1,44 +1,35 @@
 function solution(numbers, hand) {
-  const LEFT_SIDE = [1, 4, 7, '*'];
-  const MIDDLE = [2, 5, 8, 0];
-  const RIGHT_SIDE = [3, 6, 9, '#'];
-  let answer = '';
-  let currentLeft = [0, 3];
-  let currentRight = [2, 3];
-
-  for (let i = 0; i < numbers.length; i++) {
-    if (LEFT_SIDE.includes(numbers[i])) {
-      answer += 'L';
-      const leftIndex = LEFT_SIDE.findIndex((key) => key == numbers[i]);
-      currentLeft = [0, leftIndex];
-    } else if (RIGHT_SIDE.includes(numbers[i])) {
-      answer += 'R';
-      const rightIndex = RIGHT_SIDE.findIndex((key) => key == numbers[i]);
-      currentRight = [2, rightIndex];
-    } else {
-      const middleIndex = MIDDLE.findIndex((key) => key == numbers[i]);
-      const currentMiddle = [1, middleIndex];
-      const leftSpace =
-        Math.abs(currentLeft[0] - currentMiddle[0]) + Math.abs(currentLeft[1] - currentMiddle[1]);
-      const rightSpace =
-        Math.abs(currentRight[0] - currentMiddle[0]) + Math.abs(currentRight[1] - currentMiddle[1]);
-      if (leftSpace < rightSpace) {
-        answer += 'L';
-        currentLeft = [...currentMiddle];
-      } else if (leftSpace > rightSpace) {
-        answer += 'R';
-        currentRight = [...currentMiddle];
-      } else {
-        if (hand == 'left') {
-          answer += 'L';
-          currentLeft = [...currentMiddle];
-        } else {
-          answer += 'R';
-          currentRight = [...currentMiddle];
-        }
+  const leftRegex = /[147]/;
+  const rightRegex = /[369]/;
+  const direction = hand === 'left' ? 'L' : 'R';
+  let position = [1, 4, 4, 4, 3, 3, 3, 2, 2, 2];
+  let currentHandPosition = { left: [1, 1], right: [1, 1] };
+  return numbers
+    .map((number) => {
+      if (leftRegex.test(number)) {
+        currentHandPosition.left = [position[number], 1];
+        return 'L';
       }
-    }
-  }
+      if (rightRegex.test(number)) {
+        currentHandPosition.right = [position[number], 1];
+        return 'R';
+      }
 
-  return answer;
+      let distanceLeft =
+        Math.abs(position[number] - currentHandPosition.left[0]) + currentHandPosition.left[1];
+      let distanceRight =
+        Math.abs(position[number] - currentHandPosition.right[0]) + currentHandPosition.right[1];
+
+      if (distanceLeft < distanceRight) {
+        currentHandPosition.left = [position[number], 0];
+        return 'L';
+      }
+      if (distanceLeft > distanceRight) {
+        currentHandPosition.right = [position[number], 0];
+        return 'R';
+      }
+      currentHandPosition[hand] = [position[number], 0];
+      return direction;
+    })
+    .join('');
 }
