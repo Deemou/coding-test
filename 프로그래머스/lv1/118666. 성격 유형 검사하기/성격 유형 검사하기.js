9ef70484ee0240f2eb5c3d6b9283d1001524bbd0
Deemou <4106243@naver.com>
2022-12-11 00:08:06 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.57 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -1,60 +1,38 @@
 function solution(survey, choices) {
-  const test = new PersonalityTypeTest();
-  return test.getSurveyResult(survey, choices);
+  const indicators = [
+    ['R', 'T'],
+    ['C', 'F'],
+    ['J', 'M'],
+    ['A', 'N']
+  ];
+  const indicatorsWithScore = getIndicatorsWithScore(survey, choices);
+  return indicators.reduce((answer, indicator, index) => {
+    const former = indicatorsWithScore.get(indicator[0]) || 0;
+    const latter = indicatorsWithScore.get(indicator[1]) || 0;
+    if (former >= latter) answer += indicator[0];
+    else answer += indicator[1];
+    return answer;
+  }, '');
 }
 
-class PersonalityTypeTest {
-  #indicators;
-  #results;
-  constructor() {
-    this.initIndicators();
-    this.initResults();
-  }
+function getIndicatorsWithScore(survey, choices) {
+  return survey.reduce((indicatorsWithScore, item, index) => {
+    const [indicatorIndex, score] = getResult(choices[index] - 1);
+    const totalScore = indicatorsWithScore.get(item[indicatorIndex]) || 0;
+    indicatorsWithScore.set(item[indicatorIndex], totalScore + score);
+    return indicatorsWithScore;
+  }, new Map());
+}
 
-  initIndicators() {
-    this.#indicators = [
-      ['R', 'T'],
-      ['C', 'F'],
-      ['J', 'M'],
-      ['A', 'N']
-    ];
-  }
-
-  initResults() {
-    this.#results = [
-      [0, 3],
-      [0, 2],
-      [0, 1],
-      [0, 0],
-      [1, 1],
-      [1, 2],
-      [1, 3]
-    ];
-  }
-
-  getSurveyResult(survey, choices) {
-    const indicatorsWithScore = this.getIndicatorsWithScore(survey, choices);
-    return this.#indicators.reduce((answer, indicator) => {
-      const former = indicator[0];
-      const latter = indicator[1];
-      const formerScore = indicatorsWithScore.get(former) || 0;
-      const latterScore = indicatorsWithScore.get(latter) || 0;
-      if (formerScore >= latterScore) answer += former;
-      else answer += latter;
-      return answer;
-    }, '');
-  }
-
-  getIndicatorsWithScore(survey, choices) {
-    return survey.reduce((indicatorsWithScore, item, index) => {
-      const [indicatorIndex, score] = this.getResult(choices[index] - 1);
-      const totalScore = indicatorsWithScore.get(item[indicatorIndex]) || 0;
-      indicatorsWithScore.set(item[indicatorIndex], totalScore + score);
-      return indicatorsWithScore;
-    }, new Map());
-  }
-
-  getResult(choice) {
-    return this.#results[choice];
-  }
+function getResult(choice) {
+  const results = [
+    [0, 3],
+    [0, 2],
+    [0, 1],
+    [0, 0],
+    [1, 1],
+    [1, 2],
+    [1, 3]
+  ];
+  return results[choice];
 }
