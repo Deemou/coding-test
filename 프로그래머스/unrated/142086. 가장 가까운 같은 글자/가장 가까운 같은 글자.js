b211fd05b619775f4ce8c4676c6fb3cec431ce19
Deemou <4106243@naver.com>
2022-12-16 11:30:35 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 4.00 ms, Memory: 37.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -0,0 +1,14 @@
+function solution(s) {
+  const answer = [];
+  for (let i = 0; i < s.length; i++) {
+    let count = -1;
+    for (let j = i - 1; j >= 0; j--) {
+      if (s[i] === s[j]) {
+        count = i - j;
+        break;
+      }
+    }
+    answer.push(count);
+  }
+  return answer;
+}
