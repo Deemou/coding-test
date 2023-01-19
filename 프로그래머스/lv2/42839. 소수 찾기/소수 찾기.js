ff13990d20d905d8eb9fbb7116158e77d8fbf002
Deemou <yepchani@gmail.com>
2023-01-19 22:16:21 +0900

    [level 2] Title: 소수 찾기, Time: 0.77 ms, Memory: 33.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.js
@@ -0,0 +1,35 @@
+function solution(numbers) {
+  let arr = [];
+  const numArr = numbers.split('').map((v) => Number(v));
+
+  for (let i = 1; i <= numbers.length; i++) {
+    const permutations = getPermutations(numArr, i);
+    arr = arr.concat(permutations.map((v) => Number(v)).filter((v) => isPrime(v)));
+  }
+
+  const set = new Set([...arr]);
+  return set.size;
+}
+
+function getPermutations(numbers, length) {
+  const arr = [];
+  if (length === 1) return numbers;
+
+  numbers.forEach((fixed, idx) => {
+    const rest = numbers.filter((_, index) => idx !== index);
+    const permutations = getPermutations(rest, length - 1);
+    const arranged = permutations.map((v) => [fixed, v].join(''));
+    arr.push(...arranged);
+  });
+  return arr;
+}
+
+function isPrime(n) {
+  if (n < 2) return false;
+  if (n === 2) return true;
+  if (n % 2 === 0) return false;
+  for (let i = 3; i <= Math.sqrt(n); i += 2) {
+    if (n % i == 0) return false;
+  }
+  return true;
+}
