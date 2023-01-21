3da7fc9105343c95e4e639667f3477413214f75d
Deemou <yepchani@gmail.com>
2023-01-21 14:02:45 +0900

    [level 2] Title: 피로도, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/87946. 피로도/피로도.js
+++ 프로그래머스/lv2/87946. 피로도/피로도.js
@@ -1,14 +1,15 @@
 function solution(k, dungeons) {
   let answer = 0;
-  const filtered = dungeons.slice().filter((v) => v[0] <= k);
+  const MINIMUM = 0;
+  const USED = 1;
+  const possibleDungeons = dungeons.slice().filter((v) => v[MINIMUM] <= k);
 
-  for (let i = 0; i < filtered.length; i++) {
-    const subAnswer = solution(
-      k - filtered[i][1],
-      filtered.filter((_, idx) => i !== idx)
-    );
-    if (subAnswer + 1 > answer) answer = subAnswer + 1;
-    if (answer >= dungeons.length) return answer;
+  for (let i = 0; i < possibleDungeons.length; i++) {
+    const energy = k - possibleDungeons[i][USED];
+    const rest = possibleDungeons.filter((_, idx) => i !== idx);
+    const subAnswer = solution(energy, rest) + 1;
+    if (subAnswer > answer) answer = subAnswer;
+    if (answer === dungeons.length) return answer;
   }
   return answer;
 }
