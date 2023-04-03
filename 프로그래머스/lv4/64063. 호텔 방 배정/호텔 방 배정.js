ee78a7150d986ab166530d204e5cd36a727e59eb
Deemou <yepchani@gmail.com>
2023-04-03 22:12:33 +0900

    [level 4] Title: 호텔 방 배정, Time: 273.79 ms, Memory: 92.6 MB -BaekjoonHub

--- 프로그래머스/lv4/64063. 호텔 방 배정/호텔 방 배정.js
+++ 프로그래머스/lv4/64063. 호텔 방 배정/호텔 방 배정.js
@@ -1,22 +1,40 @@
 function solution(k, room_number) {
   const answer = [];
-  const map = new Map();
+  const used = new Map();
 
   room_number.forEach((num) => {
     const tmp = [];
-    let idx = num;
+    let room = num;
+
     while (true) {
-      if (!map.has(idx)) {
-        map.set(idx, idx + 1);
-        answer.push(idx);
+      if (!used.has(room)) {
+        used.set(room, room + 1);
+        answer.push(room);
+
         tmp.forEach((tmpNum) => {
-          map.set(tmpNum, idx + 1);
+          used.set(tmpNum, room + 1);
         });
         break;
       }
-      idx = map.get(idx);
-      tmp.push(idx);
+      room = used.get(room);
+      tmp.push(room);
     }
   });
   return answer;
 }
+
+//재귀
+// const solution = (k, room_number) => {
+//   const used = new Map();
+//   return room_number.map((number) => findRoom(number, used));
+// };
+
+// function findRoom(room, used) {
+//   if (used.get(room) == undefined) {
+//     used.set(room, room + 1);
+//     return room;
+//   }
+//   let next = findRoom(used.get(room), used);
+//   used.set(room, next + 1);
+//   return next;
+// }
