30660c62c8df1aa85a3b1fb2ef7f3a7fed3f9016
Deemou <yepchani@gmail.com>
2023-04-14 21:04:09 +0900

    [level 3] Title: 단어 변환, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv3/43163. 단어 변환/단어 변환.js
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.js
@@ -1,27 +1,28 @@
 function solution(begin, target, words) {
   const shouldMatch = begin.length - 1;
   const dist = Array(words.length + 1).fill(0);
+  const wordsPlus = [...words, begin];
   const queue = [];
-  const wordsPlus = words.slice();
-  wordsPlus.push(begin);
   queue.push(words.length);
 
-  while (queue.length !== 0) {
+  while (queue.length > 0) {
     const cur = queue.shift();
-    for (let i = 0; i < wordsPlus.length - 1; i++) {
+    for (let i in wordsPlus) {
       if (dist[i] !== 0) continue;
       let match = 0;
 
-      for (let j = 0; j < begin.length; j++) {
+      for (let j in begin) {
         if (wordsPlus[cur][j] === wordsPlus[i][j]) {
           match++;
         }
       }
+
       if (match !== shouldMatch) continue;
       dist[i] = dist[cur] + 1;
       if (wordsPlus[i] === target) return dist[i];
       queue.push(i);
     }
   }
+
   return 0;
 }
