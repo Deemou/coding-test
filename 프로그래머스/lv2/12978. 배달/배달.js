bcfe9d5db21bf458859e7147d9a8ebf93f202374
Deemou <yepchani@gmail.com>
2023-04-14 13:24:35 +0900

    [level 2] Title: 배달, Time: 0.59 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/lv2/12978. 배달/배달.js
+++ 프로그래머스/lv2/12978. 배달/배달.js
@@ -1,22 +1,24 @@
 function solution(N, roads, K) {
-    const distances = Array(N+1).fill(Infinity);
-    const roadmap = Array(N+1).fill().map(()=>[])
-    for(let [a, b, distance] of roads) {
-        roadmap[a].push([b, distance]);
-        roadmap[b].push([a, distance])
+  const distances = Array(N + 1).fill(Infinity);
+  const roadmap = Array(N + 1)
+    .fill()
+    .map(() => []);
+  for (let [a, b, distance] of roads) {
+    roadmap[a].push([b, distance]);
+    roadmap[b].push([a, distance]);
+  }
+
+  const queue = [[1, 0]];
+  while (queue.length > 0) {
+    const [current, time] = queue.shift();
+
+    if (distances[current] <= time) continue;
+    distances[current] = time;
+
+    for (let [next, distance] of roadmap[current]) {
+      queue.push([next, distance + time]);
     }
-    
-    const queue = [[1,0]]
-    while(queue.length > 0){
-        const [current, time] = queue.shift();
-        
-        if(distances[current] <= time) continue;
-        distances[current] = time;
-        
-        for(let [next, distance] of roadmap[current]){
-            queue.push([next, distance + time])
-        }
-    }
-    
-    return distances.filter(x=> x <= K).length;
+  }
+
+  return distances.filter((x) => x <= K).length;
 }
