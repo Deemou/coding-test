5b9609d13829c6c678cc9d017665af89400aa572
Deemou <yepchani@gmail.com>
2023-04-29 12:09:27 +0900

    [level 3] Title: 단어 변환, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv3/43163. 단어 변환/단어 변환.js
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.js
@@ -1,28 +1,58 @@
 function solution(begin, target, words) {
-  const shouldMatch = begin.length - 1;
-  const dist = Array(words.length + 1).fill(0);
-  const wordsPlus = [...words, begin];
-  const queue = [];
-  queue.push(words.length);
+  let ans = Infinity;
+  const N = words.length;
+  const visits = Array(N).fill(false);
 
-  while (queue.length > 0) {
-    const cur = queue.shift();
-    for (let i in wordsPlus) {
-      if (dist[i] !== 0) continue;
+  dfs(begin, 0);
+
+  function dfs(from, cnt) {
+    if (from === target) {
+      ans = Math.min(ans, cnt);
+      return;
+    }
+
+    for (let i = 0; i < N; i++) {
+      if (visits[i]) continue;
+      const to = words[i];
       let match = 0;
-
-      for (let j in begin) {
-        if (wordsPlus[cur][j] === wordsPlus[i][j]) {
-          match++;
-        }
+      for (let j = 0; j < to.length; j++) {
+        if (from[j] === to[j]) match++;
       }
-
-      if (match !== shouldMatch) continue;
-      dist[i] = dist[cur] + 1;
-      if (wordsPlus[i] === target) return dist[i];
-      queue.push(i);
+      if (match !== to.length - 1) continue;
+      visits[i] = true;
+      dfs(to, cnt + 1);
+      visits[i] = false;
     }
   }
 
-  return 0;
+  return ans === Infinity ? 0 : ans;
 }
+
+// function solution(begin, target, words) {
+//   const shouldMatch = begin.length - 1;
+//   const dist = Array(words.length + 1).fill(0);
+//   const wordsPlus = [...words, begin];
+//   const queue = [];
+//   queue.push(words.length);
+
+//   while (queue.length) {
+//     const cur = queue.shift();
+//     for (let i in wordsPlus) {
+//       if (dist[i] !== 0) continue;
+//       let match = 0;
+
+//       for (let j in begin) {
+//         if (wordsPlus[cur][j] === wordsPlus[i][j]) {
+//           match++;
+//         }
+//       }
+
+//       if (match !== shouldMatch) continue;
+//       dist[i] = dist[cur] + 1;
+//       if (wordsPlus[i] === target) return dist[i];
+//       queue.push(i);
+//     }
+//   }
+
+//   return 0;
+// }
