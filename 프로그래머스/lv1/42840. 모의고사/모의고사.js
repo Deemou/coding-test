6119ae87ef18179dc3893f2bb82378b5cb40d40a
Deemou <yepchani@gmail.com>
2023-04-29 10:49:39 +0900

    [level 1] Title: 모의고사, Time: 4.79 ms, Memory: 37.1 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.js
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.js
@@ -1,47 +1,20 @@
 function solution(answers) {
-  const firstPattern = [1, 2, 3, 4, 5];
-  const secondPattern = [2, 1, 2, 3, 2, 4, 2, 5];
-  const thirdPattern = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
-  const answerPatterns = [firstPattern, secondPattern, thirdPattern];
+    const answer = [];
+    const pattern1 = [1, 2, 3, 4, 5];
+    const pattern2 = [2, 1, 2, 3, 2, 4, 2, 5];
+    const pattern3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+    const count = [0, 0, 0];
 
-  const answerCounts = checkAnswersWithPatterns(answers, answerPatterns);
-  const whoGotTheMostQuestionsRight =
-    getWhoGotTheMostQuestionsRight(answerCounts);
+    for(let i in answers) {
+        if(answers[i] == pattern1[i % pattern1.length]) count[0]++;
+        if(answers[i] == pattern2[i % pattern2.length]) count[1]++;
+        if(answers[i] == pattern3[i % pattern3.length]) count[2]++;
+    }
 
-  return whoGotTheMostQuestionsRight;
-}
+    const max = Math.max(...count);
+    for(let i = 0; i < count.length; i++) {
+        if(max == count[i]) answer.push(i + 1);
+    }
 
-function checkAnswersWithPatterns(answers, answerPatterns) {
-  return answerPatterns.reduce((answerCounts, answerPattern) => {
-    const answerCount = checkAnswersWithPattern(answers, answerPattern);
-    answerCounts.push(answerCount);
-    return answerCounts;
-  }, []);
-}
-
-function checkAnswersWithPattern(answers, answerPattern) {
-  return answers.reduce((answerCount, answer, index) => {
-    const answerPatternIndex = index % answerPattern.length;
-    if (answerPattern[answerPatternIndex] == answer) answerCount++;
-    return answerCount;
-  }, 0);
-}
-
-function getWhoGotTheMostQuestionsRight(answerCounts) {
-  const maxCount = getMaxCount(answerCounts);
-  return answerCounts.reduce(
-    (whoGotTheMostQuestionsRight, answerCount, index) => {
-      if (answerCount == maxCount) whoGotTheMostQuestionsRight.push(index + 1);
-      return whoGotTheMostQuestionsRight;
-    },
-    []
-  );
-}
-
-function getMaxCount(answerCounts) {
-  let maxCount = answerCounts[0];
-  answerCounts.forEach((answerCount) => {
-    if (answerCount > maxCount) maxCount = answerCount;
-  });
-  return maxCount;
+    return answer;
 }
