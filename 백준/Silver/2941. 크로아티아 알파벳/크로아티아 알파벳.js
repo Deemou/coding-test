09911d20789f9a432b7bea06552e41e4cd16c811
Deemou <yepchani@gmail.com>
2023-07-11 23:56:33 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 120 ms, Memory: 9352 KB -BaekjoonHub

---
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./example.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+let croatia = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="];
+
+function solution(input) {
+  for (let alphabet of croatia) {
+    input = input.split(alphabet).join("K");
+  }
+
+  return input.length;
+}
+
+console.log(solution(input[0]));
