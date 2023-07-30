fcff0ef3da942e20483d297b86aeeda0b2af0685
Deemou <yepchani@gmail.com>
2023-07-30 22:42:35 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 372 ms, Memory: 29560 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.js
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.js
@@ -15,29 +15,30 @@ function solution() {
   console.log(dist[k]);
 
   function bfs() {
-    const queue = [[n, 0]];
+    const queue = [[n]];
     dist[n] = 0;
     let front = 0;
 
     while (queue.length !== front) {
-      const [cx, cnt] = queue[front];
+      const [cx] = queue[front];
       front++;
 
       const xBack = cx - 1;
       const xNext = cx + 1;
       const xJump = cx * 2;
-      if (xBack >= 0 && dist[xBack] > cnt) {
-        dist[xBack] = cnt + 1;
-        queue.push([xBack, cnt + 1]);
+
+      if (xBack >= 0 && dist[xBack] > dist[cx]) {
+        dist[xBack] = dist[cx] + 1;
+        queue.push([xBack, dist[cx] + 1]);
       }
       if (cx > k) continue;
-      if (xNext <= limit && dist[xNext] > cnt) {
-        dist[xNext] = cnt + 1;
-        queue.push([xNext, cnt + 1]);
+      if (xNext <= limit && dist[xNext] > dist[cx]) {
+        dist[xNext] = dist[cx] + 1;
+        queue.push([xNext, dist[cx] + 1]);
       }
-      if (xJump <= limit && dist[xJump] > cnt) {
-        dist[xJump] = cnt;
-        queue.push([xJump, cnt]);
+      if (xJump <= limit && dist[xJump] > dist[cx]) {
+        dist[xJump] = dist[cx];
+        queue.push([xJump, dist[cx]]);
       }
     }
   }
