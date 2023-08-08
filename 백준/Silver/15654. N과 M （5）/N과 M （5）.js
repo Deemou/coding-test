078527ef3dc61d9935fe34b40b691caadf7353a8
Deemou <yepchani@gmail.com>
2023-08-08 22:35:26 +0900

    [Silver III] Title: N과 M (5), Time: 380 ms, Memory: 47768 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.js
@@ -0,0 +1,39 @@
+function solution() {
+  const fs = require("fs");
+  const filePath =
+    process.platform === "linux" ? "/dev/stdin" : "./example.txt";
+  const input = fs
+    .readFileSync(filePath)
+    .toString()
+    .trim()
+    .split("\n")
+    .map((v) => v.split(" ").map(Number));
+
+  const [n, m] = input[0];
+  const nums = input[1].sort((a, b) => a - b);
+  const ans = [];
+  const isUsed = new Array(10001);
+
+  func(0, []);
+
+  return ans.join("\n");
+
+  function func(k, p) {
+    if (k === m) {
+      let str = "";
+      for (let i = 0; i < k; i++) {
+        str += p[i] + " ";
+      }
+      ans.push(str);
+      return;
+    }
+    for (let i = 0; i < n; i++) {
+      if (isUsed[nums[i]]) continue;
+      isUsed[nums[i]] = true;
+      func(k + 1, [...p, nums[i]]);
+      isUsed[nums[i]] = false;
+    }
+  }
+}
+
+console.log(solution());
