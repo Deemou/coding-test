389036885900360cf8bb2a76f8ae8cdbafa3b07c
Deemou <yepchani@gmail.com>
2023-08-23 16:47:46 +0900

    [Silver V] Title: 돌 게임, Time: 220 ms, Memory: 9508 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./example.txt";
+const readline = require("readline");
+let line = 0;
+let stdin = [];
+const input = () => stdin[line++];
+
+function readFile(filePath) {
+  const readStream = fs.createReadStream(filePath);
+  const rl = readline.createInterface({
+    input: readStream,
+  });
+
+  rl.on("line", (line) => {
+    stdin.push(line);
+  }).on("close", () => {
+    console.log(solution());
+    process.exit();
+  });
+}
+
+readFile(filePath);
+
+function solution() {
+  const n = +input();
+  return n % 2 === 0 ? "CY" : "SK";
+}
