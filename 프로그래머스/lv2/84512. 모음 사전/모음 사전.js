1f8960fb21a2562c59147eb322aa82990c2c8e51
Deemou <yepchani@gmail.com>
2023-09-08 01:02:12 +0900

    [level 2] Title: 모음 사전, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/84512. 모음 사전/모음 사전.js
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.js
@@ -1,18 +1,14 @@
-const dict = new Map();
-let vowel = ["A", "E", "I", "O", "U"];
-let idx = 0;
-
-function dfs(str, length) {
-  if (length > 5) return;
-
-  dict.set(str, idx++);
-  for (let i = 0; i < 5; i++) {
-    let next = str + vowel[i];
-    dfs(next, length + 1);
-  }
-}
-
 function solution(word) {
-  dfs("", 0);
-  return dict.get(word);
+  const vowels = ["A", "E", "I", "O", "U"];
+  let answer = 0;
+  // 각 자릿수별로 가능한 조합의 수. 누적합
+  let placeValue = [781, 156, 31, 6, 1];
+
+  for (let i = 0; i < word.length; i++) {
+    // 각 문자의 위치와 그 문자가 있는 자릿수별 가중치를 곱하고
+    // 마지막으로 현재 자릿수까지 포함시키기 위해 +1
+    answer += vowels.indexOf(word[i]) * placeValue[i] + 1;
+  }
+
+  return answer;
 }
