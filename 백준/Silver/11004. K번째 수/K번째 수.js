4d5babd3273357e7accbe032badccc17e52e269c
Deemou <yepchani@gmail.com>
2023-10-11 07:35:26 +0900

    [Silver V] Title: K번째 수, Time: 5024 ms, Memory: 563632 KB -BaekjoonHub

---
+++ 백준/Silver/11004. K번째 수/K번째 수.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const readline = require("readline");
+let line = 0;
+let stdin = [];
+const input = () => stdin[line++];
+
+readFile(filePath);
+
+function readFile(filePath) {
+  const readStream = fs.createReadStream(filePath);
+  const rl = readline.createInterface({
+    input: readStream,
+  });
+
+  rl.on("line", (line) => {
+    stdin.push(line);
+  }).on("close", () => {
+    console.log(solution());
+    process.exit();
+  });
+}
+
+function solution() {
+  const [N, K] = input().split(" ").map(Number);
+  const nums = input()
+    .split(" ")
+    .map(Number)
+    .sort((a, b) => a - b);
+  return nums[K - 1];
+}
