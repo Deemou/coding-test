a750715157b5a15592682baf86689065fde1b7aa
Deemou <yepchani@gmail.com>
2023-10-10 18:59:16 +0900

    [Silver V] Title: 영화감독 숌, Time: 452 ms, Memory: 23424 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const readline = require("readline");
+let line = 0;
+let stdin = [];
+const input = () => stdin[line++];
+
+readFile(filePath);
+
+function readFile(filePath) {
+  const readStream = fs.createReadStream(filePath);
+  const rl = readline.createInterface({
+    input: readStream,
+  });
+
+  rl.on("line", (line) => {
+    stdin.push(line);
+  }).on("close", () => {
+    console.log(solution());
+    process.exit();
+  });
+}
+
+function solution() {
+  const N = +input();
+  let num = 666;
+  let count = 0;
+
+  while (true) {
+    if (num.toString().includes("666")) {
+      count++;
+    }
+    if (count === N) break;
+    num++;
+  }
+
+  return num;
+}
