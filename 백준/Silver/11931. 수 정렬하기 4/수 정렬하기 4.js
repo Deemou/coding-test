f4140421f9a76afc5677c5e0f70b12a9f79828da
Deemou <yepchani@gmail.com>
2023-10-16 21:59:12 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 1776 ms, Memory: 239240 KB -BaekjoonHub

---
+++ 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.js
@@ -0,0 +1,35 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const readline = require("readline");
+let line = 0;
+let stdin = [];
+const input = () => stdin[line++];
+
+readFile(filePath);
+
+function readFile(filePath) {
+  const readStream = fs.createReadStream(filePath);
+  const rl = readline.createInterface({
+    input: readStream,
+  });
+
+  rl.on("line", (line) => {
+    stdin.push(line);
+  }).on("close", () => {
+    console.log(solution());
+    process.exit();
+  });
+}
+
+function solution() {
+  const n = +input();
+  const nums = [];
+
+  for (let i = 0; i < n; i++) {
+    nums.push(+input());
+  }
+
+  nums.sort((a, b) => b - a);
+
+  return nums.join("\n");
+}
