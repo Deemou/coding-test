cbbcb0a3f6ad0fa56d6fdde6161c2985d29c4bb3
Deemou <yepchani@gmail.com>
2024-03-20 22:15:57 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -0,0 +1,17 @@
+def solution(n):
+    x = n
+    # 1. 가장 낮은 자리의 '1' 찾기
+    right_one = x & -x
+    # 2. 가장 낮은 자리의 '1'에 1 더하기
+    added_one_bit = x + right_one
+    # 3. 변경된 비트 패턴 찾기
+    modified_pattern = x ^ added_one_bit
+    # 4. 변경된 비트 패턴 조정하기
+    # 4-1. 오른쪽 끝으로 시프트하기
+    adjusted_pattern = (modified_pattern) // right_one 
+    # 4-2. 오른쪽으로 2비트 시프트하기
+    adjusted_pattern >>= 2
+    # 5. 최종 결과 구하기
+    next_n = added_one_bit | adjusted_pattern
+    
+    return next_n
