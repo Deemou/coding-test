f64a37b7b10ef6dd5161ff83ca67ae71c5e317d0
Deemou <yepchani@gmail.com>
2024-03-20 09:23:32 +0900

    [level 1] Title: 음양 더하기, Time: 0.06 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
@@ -0,0 +1,8 @@
+def solution(absolutes, signs):
+    total_sum = 0
+    for absolute, sign in zip(absolutes, signs):
+        if sign:
+            total_sum += absolute
+        else:
+            total_sum -= absolute
+    return total_sum
