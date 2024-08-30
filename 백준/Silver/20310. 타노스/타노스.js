e1c52ff5089615e672654a3944a6c708041bc815
Deemou <yepchani@gmail.com>
2024-08-30 21:15:25 +0900

    [Silver III] Title: 타노스, Time: 108 ms, Memory: 9460 KB, Score: 25 point -BaekjoonHub

---
+++ 백준/Silver/20310. 타노스/타노스.js
@@ -0,0 +1,35 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const readline = require("readline");
+let line = 0;
+const lines = [];
+const input = () => lines[line++];
+
+function readFile(filePath) {
+  const readStream = fs.createReadStream(filePath);
+  const rl = readline.createInterface({
+    input: readStream,
+  });
+
+  rl.on("line", (line) => {
+    lines.push(line);
+  }).on("close", () => {
+    console.log(solution());
+    process.exit();
+  });
+}
+
+readFile(filePath);
+
+function solution() {
+  const str = input();
+  let zeroCnt = 0,
+    oneCnt = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === "0") zeroCnt++;
+    else oneCnt++;
+  }
+
+  return "0".repeat(zeroCnt / 2) + "1".repeat(oneCnt / 2);
+}
