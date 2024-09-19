b41df66397c68168080b981008d01ff15e205358
Deemou <yepchani@gmail.com>
2024-09-20 04:17:35 +0900

    [Bronze III] Title: 사과, Time: 116 ms, Memory: 9772 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/10833. 사과/사과.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const readline = require("readline");
+let line = 0;
+const lines = [];
+const input = () => lines[line++];
+
+function readFile(filePath) {
+  const readStream = fs.createReadStream(filePath);
+  const rl = readline.createInterface({
+    input: readStream,
+  });
+
+  rl.on("line", (line) => {
+    lines.push(line);
+  }).on("close", () => {
+    console.log(solution());
+    process.exit();
+  });
+}
+
+readFile(filePath);
+
+function solution() {
+  const N = Number(input());
+  let sum = 0;
+
+  for (let i = 0; i < N; i++) {
+    const [a, b] = input().split(" ").map(Number);
+    sum += b % a;
+  }
+
+  return sum;
+}
