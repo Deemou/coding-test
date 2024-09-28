f58a6189a1bf9c25408374cb05801f01da1424f6
Deemou <yepchani@gmail.com>
2024-09-28 22:23:10 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 168 ms, Memory: 10044 KB -BaekjoonHub

---
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const readline = require("readline");
+let line = 0;
+const lines = [];
+const input = () => lines[line++];
+
+function readFile(filePath) {
+  const readStream = fs.createReadStream(filePath);
+  const rl = readline.createInterface({
+    input: readStream,
+  });
+
+  rl.on("line", (line) => {
+    lines.push(line);
+  }).on("close", () => {
+    console.log(solution());
+    process.exit();
+  });
+}
+
+readFile(filePath);
+
+function solution() {
+  const n = Number(input());
+  const nums = input().split(" ").map(Number);
+  const dp = nums.slice();
+
+  for (let i = 1; i < n; i++) {
+    for (let j = 0; j < i; j++) {
+      if (nums[j] < nums[i]) dp[i] = Math.max(dp[i], dp[j] + nums[i]);
+    }
+  }
+
+  return Math.max(...dp);
+}
