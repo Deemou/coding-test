29a7fd015e03d8c3ddf4bdcee22a501c022da5e4
Deemou <yepchani@gmail.com>
2024-12-01 22:40:11 +0900

    [level 2] Title: 모음 사전, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/README.md
+++ 프로그래머스/2/84512. 모음 사전/README.md
@@ -1,10 +1,10 @@
 # [level 2] 모음 사전 - 84512 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512?language=javascript) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 0.00 ms
+메모리: 33.4 MB, 시간: 0.15 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 26일 17:04:22
+2024년 12월 01일 22:40:09
 
 ### 문제 설명
 
---
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.js
@@ -0,0 +1,23 @@
+function solution(word) {
+  const vowels = ["A", "E", "I", "O", "U"];
+  const MAX_LENGTH = 5;
+  const VOWELS_COUNT = 5;
+  const placeValues = getPlaceValues(MAX_LENGTH, VOWELS_COUNT);
+  let answer = 0;
+
+  for (let i = 0; i < word.length; i++) {
+    answer += vowels.indexOf(word[i]) * placeValues[i] + 1;
+  }
+
+  return answer;
+}
+
+function getPlaceValues(length, caseCount) {
+  const placeValues = [1];
+  for (let i = 1; i < length; i++) {
+    const combCounts = placeValues[i - 1] + Math.pow(caseCount, i);
+    placeValues.push(combCounts);
+  }
+
+  return placeValues.reverse();
+}
