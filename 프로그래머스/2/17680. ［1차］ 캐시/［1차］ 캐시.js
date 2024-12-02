a2eeb58d2ac677a8afd5b209448f93011ff3ba66
Deemou <yepchani@gmail.com>
2024-12-03 03:55:27 +0900

    [level 2] Title: [1차] 캐시, Time: 31.75 ms, Memory: 42.7 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
@@ -1,24 +1,24 @@
 function solution(cacheSize, cities) {
   const CACHE_HIT = 1;
   const CACHE_MISS = 5;
-  const cache = new Set();
-  const cacheQueue = [];
+  const cache = [];
+  const citySet = new Set();
 
   if (cacheSize === 0) return cities.length * CACHE_MISS;
 
   return cities.reduce((time, city) => {
     city = city.toLowerCase();
-    if (cache.has(city)) {
-      cacheQueue.splice(cacheQueue.indexOf(city), 1);
-      cacheQueue.push(city);
+    if (citySet.has(city)) {
+      cache.splice(cache.indexOf(city), 1);
+      cache.push(city);
 
       return time + CACHE_HIT;
     }
 
-    if (cache.size === cacheSize) cache.delete(cacheQueue.shift());
+    if (citySet.size === cacheSize) citySet.delete(cache.shift());
 
-    cache.add(city);
-    cacheQueue.push(city);
+    citySet.add(city);
+    cache.push(city);
 
     return time + CACHE_MISS;
   }, 0);
