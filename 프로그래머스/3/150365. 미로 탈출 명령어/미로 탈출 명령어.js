21d46ea407aaac13faf313e6ffffb6502b4a41fb
Deemou <yepchani@gmail.com>
2024-12-23 22:28:49 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 25.49 ms, Memory: 38.8 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.js
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.js
@@ -1,7 +1,9 @@
 function solution(n, m, x, y, r, c, k) {
   const IMPOSSIBLE = "impossible";
-  if (isImpossible(x, y, r, c, k)) return IMPOSSIBLE;
+  // 목표 지점까지의 거리와 k 둘 중 하나가 홀수, 다른 하나가 짝수라면 도달 불가
+  if (getDistance(x, y, r, c) % 2 !== k % 2) return IMPOSSIBLE;
 
+  // 스택에 역순으로 집어넣기 위함
   const dirs = {
     u: { x: -1, y: 0 },
     r: { x: 0, y: 1 },
@@ -13,6 +15,7 @@ function solution(n, m, x, y, r, c, k) {
   while (stack.length) {
     const [cx, cy, step, path] = stack.pop();
 
+    // 남은 횟수 내로 도달이 불가능할 경우
     if (getDistance(cx, cy, r, c) > k - step) continue;
     if (step === k)
       if (cx === r && cy === c) return path;
@@ -32,8 +35,4 @@ function solution(n, m, x, y, r, c, k) {
 
 function getDistance(x, y, r, c) {
   return Math.abs(x - r) + Math.abs(y - c);
-}
-
-function isImpossible(x, y, r, c, k) {
-  return getDistance(x, y, r, c) % 2 !== k % 2;
 }
