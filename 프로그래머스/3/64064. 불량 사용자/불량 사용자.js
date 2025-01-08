12148bb350e5c108e090984e4ae4049df34129f4
Deemou <yepchani@gmail.com>
2025-01-08 13:16:04 +0900

    [level 3] Title: 불량 사용자, Time: 86.28 ms, Memory: 38.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.js
@@ -0,0 +1,35 @@
+function solution(user_id, banned_id) {
+  const bannedSet = new Set();
+  const selected = [];
+
+  dfs(0);
+
+  return bannedSet.size;
+
+  function dfs(depth) {
+    if (depth === banned_id.length) {
+      bannedSet.add([...selected].sort().toString());
+      return;
+    }
+
+    for (let i = 0; i < user_id.length; i++) {
+      if (
+        !selected.includes(user_id[i]) &&
+        checkMatch(user_id[i], banned_id[depth])
+      ) {
+        selected.push(user_id[i]);
+        dfs(depth + 1);
+        selected.pop();
+      }
+    }
+  }
+
+  function checkMatch(user, ban) {
+    if (user.length !== ban.length) return false;
+    for (let i = 0; i < user.length; i++) {
+      if (ban[i] !== "*" && user[i] !== ban[i]) return false;
+    }
+
+    return true;
+  }
+}
