375e2fff801b1878b4cb1dfea9e83b782d563ab7
Deemou <yepchani@gmail.com>
2025-01-17 07:12:52 +0900

    [level 2] Title: 후보키, Time: 4.73 ms, Memory: 36.5 MB -BaekjoonHub

--- 프로그래머스/2/42890. 후보키/후보키.js
+++ 프로그래머스/2/42890. 후보키/후보키.js
@@ -3,56 +3,33 @@ function solution(relation) {
   const numRows = relation.length;
   const numCols = relation[0].length;
 
-  for (let i = 1; i <= numCols; i++) {
-    const combinations = getCombinations(
-      Array.from({ length: numCols }, (_, idx) => idx),
-      i
-    );
-
-    for (const comb of combinations) {
-      // 유일성 검사
-      const rowsMap = new Set();
-
-      for (let r = 0; r < numRows; r++) {
-        const tuple = comb.map((index) => relation[r][index]).join("|");
-        rowsMap.add(tuple);
-      }
-
-      if (rowsMap.size !== numRows) continue;
-
-      // 최소성 검사
-      let isMinimal = true;
-      for (const key of candidateKeys) {
-        if (key.every((k) => comb.includes(k))) {
-          isMinimal = false;
-          break;
-        }
-      }
-
-      if (isMinimal) candidateKeys.push(comb);
+  for (let i = 1; i < 1 << numCols; i++) {
+    const comb = [];
+    for (let j = 0; j < numCols; j++) {
+      if (i & (1 << j)) comb.push(j);
     }
+
+    // 유일성 검사
+    const rowsMap = new Set();
+
+    for (let r = 0; r < numRows; r++) {
+      const tuple = comb.map((index) => relation[r][index]).join("|");
+      rowsMap.add(tuple);
+    }
+
+    if (rowsMap.size !== numRows) continue;
+
+    // 최소성 검사
+    let isMinimal = true;
+    for (const key of candidateKeys) {
+      if (key.every((k) => comb.includes(k))) {
+        isMinimal = false;
+        break;
+      }
+    }
+
+    if (isMinimal) candidateKeys.push(comb);
   }
 
   return candidateKeys.length;
-}
-
-function getCombinations(array, length) {
-  const subArray = [];
-  const result = [];
-
-  function combine(start) {
-    if (subArray.length === length) {
-      result.push([...subArray]);
-      return;
-    }
-
-    for (let i = start; i < array.length; i++) {
-      subArray.push(array[i]);
-      combine(i + 1);
-      subArray.pop();
-    }
-  }
-
-  combine(0);
-  return result;
 }
