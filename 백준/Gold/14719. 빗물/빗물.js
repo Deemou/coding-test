5bef49e8d6b42a8c38bcabea789af6f85188320e
Deemou <yepchani@gmail.com>
2025-02-14 00:07:41 +0900

    [Gold V] Title: 빗물, Time: 108 ms, Memory: 9768 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.js
+++ 백준/Gold/14719. 빗물/빗물.js
@@ -24,27 +24,25 @@ readFile(filePath);
 function solution() {
   const [H, W] = input().split(" ").map(Number);
   const blocks = input().split(" ").map(Number);
-  const stack = [];
+
+  const leftMax = Array(W).fill(0);
+  const rightMax = Array(W).fill(0);
   let totalWater = 0;
 
+  let maxHeightLeft = blocks[0];
   for (let i = 0; i < W; i++) {
-    const currentHeight = blocks[i];
-    let lastFilledHeight = 0;
+    maxHeightLeft = Math.max(maxHeightLeft, blocks[i]);
+    leftMax[i] = maxHeightLeft;
+  }
 
-    while (stack.length) {
-      const [lastIndex, lastHeight] = stack.at(-1);
-      const waterWidth = i - lastIndex - 1;
-      const waterHeight =
-        Math.min(currentHeight, lastHeight) - lastFilledHeight;
-      const amount = waterWidth * waterHeight;
-      totalWater += amount;
-      lastFilledHeight += waterHeight;
+  let maxHeightRight = blocks[W - 1];
+  for (let i = W - 1; i >= 0; i--) {
+    maxHeightRight = Math.max(maxHeightRight, blocks[i]);
+    rightMax[i] = maxHeightRight;
+  }
 
-      if (lastHeight <= currentHeight) stack.pop();
-      if (lastHeight >= currentHeight) break;
-    }
-
-    stack.push([i, currentHeight]);
+  for (let i = 0; i < W; i++) {
+    totalWater += Math.min(leftMax[i], rightMax[i]) - blocks[i];
   }
 
   return totalWater;
