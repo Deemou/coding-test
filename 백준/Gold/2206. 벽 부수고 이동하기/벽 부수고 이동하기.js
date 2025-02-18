69ab097a9ee377195dabfda1c50fc7267c283056
Deemou <yepchani@gmail.com>
2025-02-18 12:52:53 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 1612 ms, Memory: 160444 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.js
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.js
@@ -1,43 +1,89 @@
-function solution() {
-  const fs = require("fs");
-  const filePath =
-    process.platform === "linux" ? "/dev/stdin" : "./example.txt";
-  const input = fs.readFileSync(filePath).toString().trim().split("\n");
-  const [n, m] = input.shift().split(" ").map(Number);
-  const dx = [0, -1, 0, 1];
-  const dy = [1, 0, -1, 0];
-  const dist = Array.from({ length: n }).map(() =>
-    Array.from({ length: m }).map(() => [0, 0])
-  );
-  dist[0][0][0] = 1;
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const readline = require("readline");
+let line = 0;
+const lines = [];
+const input = () => lines[line++];
 
-  const ans = bfs(0, 0);
-  console.log(ans);
+function readFile(filePath) {
+  const readStream = fs.createReadStream(filePath);
+  const rl = readline.createInterface({
+    input: readStream,
+  });
 
-  function bfs(x, y) {
-    const queue = [[x, y, 0]];
-    let front = 0;
-    while (queue.length !== front) {
-      const [cx, cy, w] = queue[front];
-      front++;
-      if (cx === n - 1 && cy === m - 1) return dist[cx][cy][w];
-      for (let i = 0; i < 4; i++) {
-        const nx = cx + dx[i];
-        const ny = cy + dy[i];
-        if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-        if (dist[nx][ny][w]) continue;
-        if (input[nx][ny] === "0") {
-          dist[nx][ny][w] = dist[cx][cy][w] + 1;
-          queue.push([nx, ny, w]);
-        } else {
-          if(w === 1) continue;
-          dist[nx][ny][1] = dist[cx][cy][0] + 1;
-          queue.push([nx, ny, 1]);
-        }
+  rl.on("line", (line) => {
+    lines.push(line);
+  }).on("close", () => {
+    console.log(solution());
+    process.exit();
+  });
+}
+
+readFile(filePath);
+
+class Queue {
+  constructor() {
+    this.inbox = [];
+    this.outbox = [];
+  }
+
+  enqueue(data) {
+    this.inbox.push(data);
+  }
+
+  dequeue() {
+    if (!this.outbox.length) {
+      while (this.inbox.length) {
+        this.outbox.push(this.inbox.pop());
       }
     }
-    return -1;
+
+    return this.outbox.pop();
+  }
+
+  isEmpty() {
+    return this.size() === 0;
+  }
+
+  size() {
+    return this.inbox.length + this.outbox.length;
   }
 }
 
-solution();
+function solution() {
+  const BREAK_LIMIT = 1;
+
+  const [n, m] = input().split(" ").map(Number);
+  const board = Array.from({ length: n }, () => input().split("").map(Number));
+  const dx = [0, -1, 0, 1];
+  const dy = [1, 0, -1, 0];
+  const dist = Array.from({ length: n }, () =>
+    Array.from({ length: m }, () => Array(2))
+  );
+  dist[0][0][0] = 1;
+  const queue = new Queue();
+  queue.enqueue([0, 0, 0]);
+
+  while (!queue.isEmpty()) {
+    const [cx, cy, w] = queue.dequeue();
+    if (cx === n - 1 && cy === m - 1) return dist[cx][cy][w];
+
+    for (let i = 0; i < 4; i++) {
+      const nx = cx + dx[i];
+      const ny = cy + dy[i];
+
+      if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+      if (dist[nx][ny][w]) continue;
+
+      if (board[nx][ny] === 0) {
+        dist[nx][ny][w] = dist[cx][cy][w] + 1;
+        queue.enqueue([nx, ny, w]);
+      } else if (w < BREAK_LIMIT) {
+        dist[nx][ny][w + 1] = dist[cx][cy][w] + 1;
+        queue.enqueue([nx, ny, w + 1]);
+      }
+    }
+  }
+
+  return -1;
+}
