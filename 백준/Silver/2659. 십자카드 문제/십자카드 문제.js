1d52def54f80c22f55d1b2203a1b944515339e76
Deemou <yepchani@gmail.com>
2025-03-16 23:46:55 +0900

    [Silver III] Title: 십자카드 문제, Time: 188 ms, Memory: 12232 KB -BaekjoonHub

--- 백준/Silver/2659. 십자카드 문제/십자카드 문제.js
+++ 백준/Silver/2659. 십자카드 문제/십자카드 문제.js
@@ -24,43 +24,26 @@ readFile(filePath);
 function solution() {
   const target = input().split(" ").join("");
   const targetClockNum = getClockNum(target);
-  const allClockNums = getAllClockNums();
-  return allClockNums.indexOf(targetClockNum) + 1;
-}
 
-function getClockNum(num) {
-  let min = Number(num);
-  for (let i = 0; i < 4; i++) {
-    const n = Number(num.slice(i) + num.slice(0, i));
-    min = Math.min(min, n);
+  let cnt = 0;
+  for (let i = 1111; i <= Number(targetClockNum); i++) {
+    if (isClockNumber(i)) cnt++;
   }
-  return min.toString();
-}
 
-function getAllClockNums() {
-  const allNums = getAllNums(4).map((v) => v.join(""));
-  const allClockNums = new Set();
-  allNums.forEach((num) => {
-    const clockNum = getClockNum(num);
-    allClockNums.add(clockNum);
-  });
-  return [...allClockNums].sort();
-}
+  return cnt;
 
-function getAllNums(digit) {
-  if (digit === 1) {
-    const arr = [];
-    for (let i = 1; i <= 9; i++) {
-      arr.push([i]);
+  function getClockNum(num) {
+    let min = Number(num);
+    for (let i = 0; i < 4; i++) {
+      const n = Number(num.slice(i) + num.slice(0, i));
+      min = Math.min(min, n);
     }
-    return arr;
+    return min.toString();
   }
 
-  const arr = [];
-  for (let i = 1; i <= 9; i++) {
-    const nums = getAllNums(digit - 1);
-    const nn = nums.map((v) => [i, ...v]);
-    arr.push(...nn);
+  function isClockNumber(n) {
+    const s = n.toString();
+    if (s.length !== 4 || s.includes("0")) return false;
+    return getClockNum(s) === n.toString();
   }
-  return arr;
 }
