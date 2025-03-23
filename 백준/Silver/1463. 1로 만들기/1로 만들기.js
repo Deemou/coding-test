79044a4e4f9e748760a8729dc4aa13a824ff7302
Deemou <yepchani@gmail.com>
2025-03-23 15:37:26 +0900

    [Silver III] Title: 1로 만들기, Time: 216 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.js
@@ -0,0 +1,35 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const readline = require("readline");
+let line = 0;
+let stdin = [];
+const input = () => stdin[line++];
+
+function readFile(filePath) {
+  const readStream = fs.createReadStream(filePath);
+  const rl = readline.createInterface({
+    input: readStream,
+  });
+
+  rl.on("line", (line) => {
+    stdin.push(line);
+  }).on("close", () => {
+    console.log(solution());
+    process.exit();
+  });
+}
+
+readFile(filePath);
+
+function solution() {
+  const N = +input();
+  const dp = Array(N + 1).fill(0);
+
+  for (let i = 2; i <= N; i++) {
+    dp[i] = dp[i - 1] + 1;
+    if (i % 2 === 0) dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+    if (i % 3 === 0) dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+  }
+
+  return dp[N];
+}
