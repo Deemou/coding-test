26aa21e30e49aba95d2f45d39f73f3f60615bdb4
Deemou <yepchani@gmail.com>
2025-05-29 21:59:57 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.03 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,11 +1,11 @@
 def solution(n, words):
-    used_words = set()
+    used = set()
     last_char = words[0][0]
 
     for i, word in enumerate(words):
-        if word in used_words or last_char != word[0]:
-            return [(i % n) + 1, (i // n) + 1]
-        used_words.add(word)
+        if last_char != word[0] or word in used:
+            return [i % n + 1, i // n + 1]
+        used.add(word)
         last_char = word[-1]
 
     return [0, 0]
